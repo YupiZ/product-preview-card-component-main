0106a5550153add2c8214e4c31594780685c9058
YupiZ <101301115+YupiZ@users.noreply.github.com>
2023-10-26 20:42:56 -0400

    final changes

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+
 const img = document.querySelector("img");
 
-if (screen.width > 1440) {
-	img.src = "images/image-product-desktop.jpg";
+// Function to update the image source based on screen width
+function updateImageBasedOnScreenWidth() {
+    if (window.innerWidth >= 1440) {
+        img.src = "images/image-product-desktop.jpg";
+    } else {
+        img.src = "images/image-product-mobile.jpg";
+    }
 }
+
+// Initial update based on screen width
+updateImageBasedOnScreenWidth();
+
+// Add a resize event listener to continuously monitor screen width
+window.addEventListener("resize", updateImageBasedOnScreenWidth);
